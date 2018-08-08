aecc6c01d85ddc234c9e6a013dcde4d2552023b1
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-08-07 23:14:13 -0300

    Create a stack navigator and a first mock screen

---
+++ src/components/ranking/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import style from './style';
+
+const Ranking = () => (
+  <View style={style.container}>
+    <Text>
+      This will be the ranking screen
+    </Text>
+  </View>
+);
+
+export default Ranking;
