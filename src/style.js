cff58d81c58309eb90adf8da879742cda0f0b258
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-08-14 23:29:23 -0300

    Increase the font size of the screen title

--- src/style.js
+++ src/style.js
@@ -5,6 +5,7 @@ import { PRIMARY_DARK, TEXT_ON_PRIMARY } from './constants/colors';
 export default StyleSheet.create({
   headerTitle: {
     color: TEXT_ON_PRIMARY,
+    fontSize: 24,
   },
   headerTitleContainer: {
     backgroundColor: PRIMARY_DARK,
