9e8031e4c068b43de3a724080a5fe10d486fc47d
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-08-14 23:18:09 -0300

    Create a title to ranking screen and apply the theme on it

---
+++ src/style.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+import { PRIMARY_DARK, TEXT_ON_PRIMARY } from './constants/colors';
+
+export default StyleSheet.create({
+  headerTitle: {
+    color: TEXT_ON_PRIMARY,
+  },
+  headerTitleContainer: {
+    backgroundColor: PRIMARY_DARK,
+  },
+});
