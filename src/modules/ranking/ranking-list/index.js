f21dd7fa7d2c90df30da6494a4f35d78c8857b08
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2018-08-17 22:40:45 -0300

    Render a message when there are no cards on ranking

--- src/modules/ranking/ranking-list/index.js
+++ src/modules/ranking/ranking-list/index.js
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 
 import RankingCard from '../ranking-card';
 import style from './style';
+
+const EmptyRanking = () => (
+  <View style={style.emptyRankingContainer}>
+    <Text style={style.emptyRankingText}>
+      {/* TODO: Internationalize the messages */}
+      Não há pontos registrados
+    </Text>
+  </View>
+);
 
 const RankingList = ({ data }) => (
   <FlatList
     data={data}
     keyExtractor={item => `${item.firstName} ${item.lastName}`}
-    // TODO: ListEmptyComponent
+    ListEmptyComponent={EmptyRanking}
     renderItem={({ item, index }) => (
       <RankingCard
         {...item}
--- src/modules/ranking/ranking-list/style.js
+++ src/modules/ranking/ranking-list/style.js
@@ -1,6 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+  emptyRankingContainer: {
+    flex: 1,
+  },
+  emptyRankingText: {
+    fontSize: 40,
+    fontWeight: '300',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
   lastElement: {
     marginBottom: 15,
   },
